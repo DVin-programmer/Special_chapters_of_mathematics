50826d74ca065cc8bbdf4210b50026cc455fe75d
VDV <programsVin@yandex.ru>
2020-10-04 21:42:42 +0300

    2 commit

--- Example_with_1.7.py
+++ Example_with_1.7.py
@@ -67,7 +67,6 @@ print('<a, b, c> =', A1)
 t1_lst = []
 μA1_lst = []
 for t1 in range(A1[0], A1[2] + 1):
-    #print('μ(' + str(t1) + ') =', μNUM_triangle(t1, A1[0], A1[1], A1[2]))
     cprint('μ(' + str(t1) + ') = ' + str(μNUM_triangle(t1, A1[0], A1[1], A1[2])), color="red")
     t1_lst.append(t1)
     μA1_lst.append(μNUM_triangle(t1, A1[0], A1[1], A1[2]))
@@ -79,7 +78,6 @@ print('<a, b, c> =', A2)
 t2_lst = []
 μA2_lst = []
 for t2 in range(A2[0], A2[2] + 1):
-    #print('μ(' + str(t2) + ') =', μNUM_triangle(t2, A2[0], A2[1], A2[2]))
     cprint('μ(' + str(t2) + ') = ' +
            str(μNUM_triangle(t2, A2[0], A2[1], A2[2])), color="yellow")
     t2_lst.append(t2)
@@ -92,7 +90,6 @@ print('<a, b, c, d> =', A3)
 t3_lst = []
 μA3_lst = []
 for t3 in range(A3[0], A3[3] + 1):
-    #print('μ(' + str(t3) + ') =', μNUM_trapezoid(t3, A3[0], A3[1], A3[2], A3[3]))
     cprint('μ(' + str(t3) + ') = ' +
            str(μNUM_trapezoid(t3, A3[0], A3[1], A3[2], A3[3])), color="green")
     t3_lst.append(t3)
@@ -107,7 +104,6 @@ print('<a, b, c> =', B1)
 s1_lst = []
 μB1_lst = []
 for s1 in range(B1[0], B1[2] + 1):
-    # print('μ(' + str(s1) + ') =', μNUM_triangle(s1, B1[0], B1[1], B1[2]))
     cprint('μ(' + str(s1) + ') = ' + \
            str(μNUM_triangle(s1, B1[0], B1[1], B1[2])), color="red")
     s1_lst.append(s1)
@@ -120,7 +116,6 @@ print('<a, b, c> =', B2)
 s2_lst = []
 μB2_lst = []
 for s2 in range(B2[0], B2[2] + 1):
-    # print('μ(' + str(s2) + ') =', μNUM_triangle(s2, B2[0], B2[1], B2[2]))
     cprint('μ(' + str(s2) + ') = ' + \
            str(μNUM_triangle(s2, B2[0], B2[1], B2[2])), color="yellow")
     s2_lst.append(s2)
@@ -134,7 +129,6 @@ print('<a, b, c> =', B3)
 s3_lst = []
 μB3_lst = []
 for s3 in range(B3[0], B3[3] + 1):
-    # print('μ(' + str(s3) + ') =', μNUM_trapezoid(s3, B3[0], B3[1], B3[2], B3[3]))
     cprint('μ(' + str(s3) + ') = ' + \
            str(μNUM_trapezoid(s3, B3[0], B3[1], B3[2], B3[3])), color="green")
     s3_lst.append(s3)
@@ -149,7 +143,6 @@ print('<a, b, c> =', D1)
 Σ1_lst = []
 μD1_lst = []
 for Σ1 in range(D1[0], D1[2] + 1):
-    # print('μ(' + str(Σ1) + ') =', μNUM_triangle(Σ1, D1[0], D1[1], D1[2]))
     cprint('μ(' + str(Σ1) + ') = ' + \
            str(μNUM_triangle(Σ1, D1[0], D1[1], D1[2])), color="red")
     Σ1_lst.append(Σ1)
@@ -163,7 +156,6 @@ print('<a, b, c> =', D2)
 Σ2_lst = []
 μD2_lst = []
 for Σ2 in range(D2[0], D2[2] + 1):
-    # print('μ(' + str(Σ2) + ') =', μNUM_triangle(Σ2, D2[0], D2[1], D2[2]))
     cprint('μ(' + str(Σ2) + ') = ' + \
            str(μNUM_triangle(Σ2, D2[0], D2[1], D2[2])), color="yellow")
     Σ2_lst.append(Σ2)
@@ -177,7 +169,6 @@ print('<a, b, c> =', D3)
 Σ3_lst = []
 μD3_lst = []
 for Σ3 in range(D3[0], D3[2] + 1):
-    # print('μ(' + str(Σ3) + ') =', μNUM_triangle(Σ3, D3[0], D3[1], D3[2]))
     cprint('μ(' + str(Σ3) + ') = ' + \
            str(μNUM_triangle(Σ3, D3[0], D3[1], D3[2])), color="yellow")
     Σ3_lst.append(Σ3)
@@ -190,7 +181,6 @@ print('<a, b, c> =', D4)
 Σ4_lst = []
 μD4_lst = []
 for Σ4 in range(D4[0], D4[3] + 1):
-    # print('μ(' + str(Σ4) + ') =', μNUM_trapezoid(Σ4, D4[0], D4[1], D4[2], D4[3]))
     cprint('μ(' + str(Σ4) + ') = ' + \
            str(μNUM_trapezoid(Σ4, D4[0], D4[1], D4[2], D4[3])), color="green")
     Σ4_lst.append(Σ4)
